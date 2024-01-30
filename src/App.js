a3132f2f4bd1f1ced9a853721f9f6ae47bcf3f86
leostelon <leostelon17@gmail.com>
2024-01-30 22:38:10 +0530

    phantom wallet util

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Index } from "./screens/Index";
+import { Signin } from "./screens/Singin";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Index />} />
+        <Route path="/" exact element={<Signin />} />
+        <Route path="/index" exact element={<Index />} />
       </Routes>
     </Router>
   );
