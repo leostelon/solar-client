035bf5fc2f8f3f9b476d0a251f277f0b334b2327
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2024-02-02 22:40:15 +0530

    token expire check

--- src/api/user.js
+++ src/api/user.js
@@ -4,45 +4,62 @@ import { getProvider } from "../utils/wallet";
 import bs58 from "bs58";
 
 export const createUser = async function (address) {
-  try {
-    const nonceResponse = await axios.post(
-      `${SERVER_URL}/user/generateNonce`,
-      { address },
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-    const formattedNonceResponse = nonceResponse.data;
-    const nonce = formattedNonceResponse.nonce;
-    const message = "Solar, click sign-in to login. \n \nNonce:\n" + nonce;
-    const provider = await getProvider();
-    const { signature, publicKey } = await provider.signMessage(
-      new TextEncoder().encode(message),
-      "utf8"
-    );
+	try {
+		const nonceResponse = await axios.post(
+			`${SERVER_URL}/user/generateNonce`,
+			{ address },
+			{
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+			}
+		);
+		const formattedNonceResponse = nonceResponse.data;
+		const nonce = formattedNonceResponse.nonce;
+		const message = "Solar, click sign-in to login. \n \nNonce:\n" + nonce;
+		const provider = await getProvider();
+		const { signature, publicKey } = await provider.signMessage(
+			new TextEncoder().encode(message),
+			"utf8"
+		);
 
-    const response = await axios.post(
-      `${SERVER_URL}/user/signin`,
-      {
-        displayName: "Unnamed",
-        public_key: publicKey.toBase58(),
-        sign: bs58.encode(signature),
-        nonce,
-      },
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": `application/json`,
-        },
-      }
-    );
-    if (response.status === 201) {
-      localStorage.setItem("token", response.data.token);
+		const response = await axios.post(
+			`${SERVER_URL}/user/signin`,
+			{
+				displayName: "Unnamed",
+				public_key: publicKey.toBase58(),
+				sign: bs58.encode(signature),
+				nonce,
+			},
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": `application/json`,
+				},
+			}
+		);
+		if (response.status === 201) {
+			localStorage.setItem("token", response.data.token);
 
-      return response.data;
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
+			return response.data;
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+
+export const getUser = async function () {
+	try {
+		const token = localStorage.getItem("token");
+		await axios.get(`${SERVER_URL}/user`, {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ` + token,
+			},
+		});
+		return 200;
+	} catch (error) {
+		if (error.response && error.response.status === 401) {
+			return 401;
+		}
+	}
 };
--- src/screens/Singin.jsx
+++ src/screens/Singin.jsx
@@ -2,33 +2,38 @@ import { Box } from "@mui/material";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { connectWalletToSite, getWalletAddress } from "../utils/wallet";
-import { createUser } from "../api/user";
+import { createUser, getUser } from "../api/user";
 
 export const Signin = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  async function connectSite() {
-    let token = localStorage.getItem("token");
-    await connectWalletToSite();
-    if (token && token !== "" && token !== "undefined") {
-      return navigate("/index");
-    }
-    const address = await getWalletAddress();
-    if (address && address !== "") {
-      if (!token || token === "" || token === "undefined") {
-        await createUser(address);
+	async function connectSite() {
+		let token = localStorage.getItem("token");
+		await connectWalletToSite();
+		if (token && token !== "" && token !== "undefined") {
+			const status = await getUser();
+      if(status === 401 ) {
+        localStorage.clear();
+        return connectSite();
       }
-      token = localStorage.getItem("token");
-      if (token && token !== "" && token !== "undefined") {
-        navigate("/index");
-      }
-    }
-  }
+			return navigate("/index");
+		}
+		const address = await getWalletAddress();
+		if (address && address !== "") {
+			if (!token || token === "" || token === "undefined") {
+				await createUser(address);
+			}
+			token = localStorage.getItem("token");
+			if (token && token !== "" && token !== "undefined") {
+				navigate("/index");
+			}
+		}
+	}
 
-  useEffect(() => {
-    connectSite();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	useEffect(() => {
+		connectSite();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return <Box>signin</Box>;
+	return <Box>signin</Box>;
 };
